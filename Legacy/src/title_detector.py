228ac2b040ca276f5fef446b0599fffe8f946934
Subhajit-Chakraborty15 <158772406+Subhajit-Chakraborty15@users.noreply.github.com>
2025-07-23 00:53:25 +0530

    Removed old files

--- Legacy/src/title_detector.py
+++
@@ -1,7 +0,0 @@
-def detect_title(pages_data):
-    # On page one, find the text with largest size
-    if not pages_data or not pages_data[0]:
-        return "Untitled Document"
-    first_page = pages_data[0]
-    largest = max(first_page, key=lambda e: e["size"])
-    return largest["text"]
